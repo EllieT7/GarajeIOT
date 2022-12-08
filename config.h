8b00f3f42ca41bad23ac5596c14ef8fe45c65bd4
EllieT7 <elizabeth.tacachira@gmail.com>
2022-12-08 13:11:00 -0400

    Imagenes y correcciones de diseño

--- config.h
+++ config.h
@@ -2,5 +2,7 @@
 //const char* password =  "XXXXXXXXXXXXXXXXXX";    //Indicamos la contraseña de de red WiFi
 //const char* ssid = "Nao";
 //const char* password =  "12345678"; 
-const char* ssid = "AXS_2.4G_b6qGEE";
-const char* password =  "PhGqXGhY";
+//const char* ssid = "AXS_2.4G_b6qGEE";
+//const char* password =  "PhGqXGhY";
+const char* ssid = "TACACHIRA.";
+const char* password =  "2600Tab876..";
